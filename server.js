2f86a260375f81996b25df9d642db63be1e64837
Glitch (glitch-hello-node) <none>
2021-09-14 06:42:19 +0000

    📇🚤 Checkpoint ./server.js:36132268/2483 ./LICENSE:36132268/8559 ./.glitch-assets:36132268/43

--- server.js
+++ server.js
@@ -1,33 +1,24 @@
-/**
-* This is the main Node.js server script for your project
-* Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
-*/
-
-import fetch from 'node-fetch';
-import path from 'path';
-import { JSDOM } from 'jsdom';
-
-// Require the fastify framework and instantiate it
-import Fastify from 'fastify'
+import fetch from "node-fetch";
+import path from "path";
+import { JSDOM } from "jsdom";
+import Fastify from "fastify";
 const fastify = Fastify({
-  logger: true
-})
+  logger: false,
+});
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+fastify.get("/:url", async function (request, reply) {
+  const response = await fetch(
 
-fastify.get("/album/:album", async function(request, reply) {
-  console.log(request.params.album);
-  console.log(`https://open.spotify.com/album/${request.params.album}`);
-  const response = await fetch(`https://open.spotify.com/album/${request.params.album}`);
-  const { document } = new JSDOM(response.body).window;
+  );
+  const { document } = new JSDOM(await response.text()).window;
   const jacketUrl = document.querySelector('meta[property="og:image"]').content;
   reply.sent = true;
-  reply.raw.end(`${request.params.album}`);
+  reply.raw.end(jacketUrl);
   return Promise.resolve();
-})
+});
 
 // Run the server and report out to the logs
-fastify.listen(process.env.PORT, function(err, address) {
+fastify.listen(process.env.PORT, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
