1096a7bc161a8939800d3c7c88a7267610bd187d
Glitch (glitch-hello-node) <none>
2021-09-14 10:38:49 +0000

    👨‍❤️‍👨🚜 Checkpoint ./server.js:36132268/2921 ./package.json:36132268/1349

--- server.js
+++ server.js
@@ -3,38 +3,36 @@ import path from "path";
 import { JSDOM } from "jsdom";
 import Fastify from "fastify";
 const fastify = Fastify({
-  logger: false,
+  logger: false
 });
 
-fastify.get("/:url", async function (request, reply) {
+fastify.get("/:url", async function(request, reply) {
   const response = await fetch(decodeURI(request.params.url));
   const { document } = new JSDOM(await response.text()).window;
-  const ogImageUrl = document.querySelector(
-    'meta[property="og:image"]'
-  ).content;
+  const ogImageUrl = document.querySelector('meta[property="og:image"]')
+    .content;
   reply.send({ ogImage: ogImageUrl });
 });
 
-fastify.get("/:url.png", async function (request, reply) {
-    const response = await fetch(decodeURI(request.params.url));
-    const { document } = new JSDOM(await response.text()).window;
-    const ogImageUrl = document.querySelector(
-      'meta[property="og:image"]'
-    ).content;
-    const imageResponse = await fetch(ogImageUrl);
-    const buffer = await imageResponse.buffer();
-    reply.send(buffer);
-  });
+fastify.get("/:urlpng", async function(request, reply) {
+  const response = await fetch(decodeURI(request.params.url));
+  const { document } = new JSDOM(await response.text()).window;
+  const ogImageUrl = document.querySelector('meta[property="og:image"]')
+    .content;
+  // const imageResponse = await fetch(ogImageUrl);
+  // const buffer = await imageResponse.buffer();
+  reply.send({"hoge"});
+});
 
-fastify.get("/", async function (request, reply) {
+fastify.get("/", async function(request, reply) {
   reply.send({
     usage:
-      "pass encoded url and reply og-image url. example: https://og-image.glitch.me/https%3A%2F%2Fopen.spotify.com%2Falbum%2F063f8Ej8rLVTz9KkjQKEMa",
+      "pass encoded url and reply og-image url. example: https://og-image.glitch.me/https%3A%2F%2Fopen.spotify.com%2Falbum%2F063f8Ej8rLVTz9KkjQKEMa"
   });
 });
 
 // Run the server and report out to the logs
-fastify.listen(process.env.PORT, function (err, address) {
+fastify.listen(process.env.PORT, function(err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
