8e8d165d3804083c8acd0f82cff2760dca689f5d
Glitch (glitch-hello-node) <none>
2021-09-14 09:40:52 +0000

    🚔💓 Checkpoint ./server.js:36132268/1989

--- server.js
+++ server.js
@@ -7,18 +7,20 @@ const fastify = Fastify({
 });
 
 fastify.get("/:url", async function (request, reply) {
-  const response = await fetch(
-    decodeURI(request.params.url)
-  );
+  const response = await fetch(decodeURI(request.params.url));
   const { document } = new JSDOM(await response.text()).window;
-  const ogImageUrl = document.querySelector('meta[property="og:image"]').content;
-  reply.send({ogImage: ogImageUrl})
+  const ogImageUrl = document.querySelector(
+    'meta[property="og:image"]'
+  ).content;
+  reply.send({ ogImage: ogImageUrl });
 });
 
 fastify.get("/", async function (request, reply) {
-  reply.send({usage: "pass encoded url and reply og-image url. example: https://og-image.glitch.me/https%3A%2F%2Fopen.spotify.com%2Falbum%2F063f8Ej8rLVTz9KkjQKEMa"});
+  reply.send({
+    usage:
+      "pass encoded url and reply og-image url. example: https://og-image.glitch.me/https%3A%2F%2Fopen.spotify.com%2Falbum%2F063f8Ej8rLVTz9KkjQKEMa",
+  });
 });
-
 
 // Run the server and report out to the logs
 fastify.listen(process.env.PORT, function (err, address) {
